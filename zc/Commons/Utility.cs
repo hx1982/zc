a9ad6cede8e59b7a65f93a28e835cf281ffd6e01
admin <admin@ADMIN-PC>
2018-05-26 00:58:47 +0800

    修改user_code生成规则

--- zc/Commons/Utility.cs
+++ zc/Commons/Utility.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using zc.Models;
 namespace zc.Commons
@@ -12,29 +14,51 @@ namespace zc.Commons
     public static class Utility
     {
         public static readonly int USER_CODE_DEFAULT = 10000;
-
+        public static Random random = new Random();
         private static object lockObj = new object();
 
         /// <summary>
-        /// 生成会员编号user_code, 策略: user数量 + 1
+        /// 生成会员编号user_code
         /// </summary>
         /// <returns></returns>
-        public static string GenerateUserCode()
+        public static string GenerateUserCode(user user)
         {
-            lock (lockObj)
+            string usercode = user.id_number.Substring(user.id_number.Length - 4)
+                + user.user_phone.Substring(user.user_phone.Length - 4);
+            usercode = usercode.Replace('x', '0').Replace('X', '0');
+            bool exists = AllreadyExsits(usercode);
+            if (exists)
             {
-                using (ZCDbContext db = new ZCDbContext())
-                {
-                    int usercode = 0;
-                    var query = db.users.Count();
-                    if (query > 0)
-                    {
-                        usercode = query;
-                    }
-                    usercode++;
-                    return usercode.ToString("00000000");
-                }
+                usercode = RandomUserCode();
             }
+            return usercode;
+        }
+
+        private static bool AllreadyExsits(string usercode)
+        {
+            bool exists = false;
+            using (ZCDbContext db = new ZCDbContext())
+            {
+                var query = db.users.Where(a => a.user_code.Equals(usercode)).Count();
+                exists = query > 0;
+            }
+            return exists;
+        }
+
+        private static string RandomUserCode()
+        {
+            string usercode;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 8; i++)
+            {
+                sb.Append(random.Next(10));
+            }
+            usercode = sb.ToString();
+            if (AllreadyExsits(usercode))
+            {
+                usercode = RandomUserCode();
+            }
+            return usercode;
         }
 
         /// <summary>
